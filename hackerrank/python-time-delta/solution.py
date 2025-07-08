0ab53bc2a19398b21541cb603b3bafc0ec489c3b
Bryan Green <bag@mac.com>
2025-07-08 13:37:00 -0700

    Initial commit of Time Delta problem.

---
+++ hackerrank/python-time-delta/solution.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+from datetime import datetime
+
+# Complete the time_delta function below.
+def time_delta(t1, t2):
+    fs = "%a %d %b %Y %H:%M:%S %z"
+    u1 = datetime.strptime(t1, fs)
+    u2 = datetime.strptime(t2, fs)
+    td = u1 - u2
+    return(str(round(td.total_seconds())))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        t1 = input()
+
+        t2 = input()
+
+        delta = time_delta(t1, t2)
+
+        fptr.write(delta + '\n')
+
+    fptr.close()
