9cfef7cd56e9df67ea90b643ea9cf3d47b09d058
Bryan Green <bag@mac.com>
2025-07-16 12:30:13 -0700

    Shorter is better, if not always easier to read.

--- leetcode/7. Reverse Integer/solution-reverse-integer.py
+++ leetcode/7. Reverse Integer/solution-reverse-integer.py
@@ -1,8 +1,4 @@
-# https://leetcode.com/problems/reverse-integer/
 class Solution:
     def reverse(self, x: int) -> int:
         r = (-1 if x < 0 else 1) * int(str(abs(x))[::-1])
-        if r < -2**31 or r > 2**31+1:
-            return(0)
-        else:
-            return(r)
+        return(0 if r < -2**31 or r > 2**31+1 else r)
