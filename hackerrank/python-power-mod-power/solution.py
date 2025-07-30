eaf7d8a9f95c1ade3b0e20e580f106f7526bc8a2
Bryan Green <bag@mac.com>
2025-07-30 12:22:46 -0700

    Two lines...

--- hackerrank/python-power-mod-power/solution.py
+++ hackerrank/python-power-mod-power/solution.py
@@ -1,6 +1,3 @@
 # https://www.hackerrank.com/challenges/python-power-mod-power/problem
-a = int(input())
-b = int(input())
-m = int(input())
-print(pow(a, b))
-print(pow(a, b, m))
+a, b, m = int(input()), int(input()), int(input())
+print(pow(a, b), pow(a, b, m), sep='\n')
