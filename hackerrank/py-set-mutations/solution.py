4f61c759e745ffbcfd8414bdb58047db791426bc
Bryan Green <bag@mac.com>
2026-06-08 14:42:35 -0700

    Make it a bit more pythonic.

--- hackerrank/py-set-mutations/solution.py
+++ hackerrank/py-set-mutations/solution.py
@@ -5,22 +5,23 @@ def process_command(A):
     argument_length = int(argument_length)
     if argument_length > 0:
         argument = set(map(int, input().split()))
-        if command == 'update':
-            A.update(argument)
-        elif command == 'symmetric_difference_update':
-            A.symmetric_difference_update(argument)
-        elif command == 'difference_update':
-            A.difference_update(argument)
-        elif command == 'intersection_update':
-            A.intersection_update(argument)
-        else:
-            print(f"Unknown command: \"{command}\".")
+        match command:
+            case 'update':
+                A.update(argument)
+            case 'symmetric_difference_update':
+                A.symmetric_difference_update(argument)
+            case 'difference_update':
+                A.difference_update(argument)
+            case 'intersection_update':
+                A.intersection_update(argument)
+            case _:
+                print(f"Unknown command: \"{command}\".")
         return A
 
 _ = int(input()) # length_A -> unused. Cound go into a log.
 A = set(map(int, input().split()))
 N = int(input())
 if N > 0:
-    for _ in range(N):
+    for _ in range(N): # Don't use n, as in "n in range".
         A = process_command(A)
 print(sum(A))
